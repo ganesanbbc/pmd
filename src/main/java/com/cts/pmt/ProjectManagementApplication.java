48cff00125079c929a070f04c3667190eea0219b
ganesanns <ganesan.ns@cognizant.com>
2018-03-26 16:29:14 +0530

    Enable Request Logging : https://www.javadevjournal.com/spring/log-incoming-requests-spring/

--- src/main/java/com/cts/pmt/ProjectManagementApplication.java
+++ src/main/java/com/cts/pmt/ProjectManagementApplication.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class ProjectManagementApplication {
@@ -16,4 +18,13 @@ public class ProjectManagementApplication {
     }
 
 
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setIncludeHeaders(false);
+        return loggingFilter;
+    }
 }
