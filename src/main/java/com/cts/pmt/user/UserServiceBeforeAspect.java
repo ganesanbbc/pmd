fc9baefa6fa5dadcbd44efda51a8fec973868a87
ganesanns <ganesan.ns@cognizant.com>
2018-03-27 11:44:48 +0530

    Enable Logging aspect by implement AOP & AspectJ 1. Add dependency - AOP & AspectJ 2. Add Before aspect 3. Add After aspect

---
+++ src/main/java/com/cts/pmt/user/UserServiceBeforeAspect.java
@@ -0,0 +1,24 @@
+package com.cts.pmt.user;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class UserServiceBeforeAspect {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    //What kind of method calls I would intercept
+    //execution(* PACKAGE.*.*(..))
+    //Weaving & Weaver
+    @Before("execution(* com.cts.pmt.user.**.*.*(..))")
+    public void before(JoinPoint joinPoint) {
+        //Advice
+        logger.info("~~~~~~~~~~Before Method Called~~~~~~~~~");
+    }
+}
